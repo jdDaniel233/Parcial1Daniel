cdbd7621c538ce84dd0c9b81f00f197507fd7b56
Daniel <simbanadaniel42@gmail.com>
2023-06-12 17:03:35 -0500

    Separar funciones por separado

--- app/src/main/java/com/example/parcial1daniel/MainActivity.java
+++ app/src/main/java/com/example/parcial1daniel/MainActivity.java
@@ -13,7 +13,7 @@ import android.widget.Toast;
 import bdd.BDHelper;
 
 public class MainActivity extends AppCompatActivity {
-    EditText et_funcionario,et_area,et_cargo,et_hijos,et_estado,et_atrasos,et_horas;
+    EditText et_funcionario,et_area,et_cargo,et_hijos,et_estado,et_atrasos,et_sueldo,et_subsidio,et_descuentoA;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +26,45 @@ public class MainActivity extends AppCompatActivity {
         et_hijos=findViewById(R.id.txtHijos);
         et_estado=findViewById(R.id.txtEstado);
         et_atrasos=findViewById(R.id.txtAtrasos);
+        et_sueldo=findViewById(R.id.txtSueldo);
+        et_subsidio=findViewById(R.id.txtSubsidio);
+        et_descuentoA=findViewById(R.id.txtDescuentoA);
     }
+
+    public double determinarSueldo(String cargo){
+        double sueldo=0.00;
+        //String cargo=et_cargo.getText().toString();
+        if (cargo.equals("Administrativo")==true) {
+            sueldo=880.00;
+        }else if(cargo.equals("Docente")==true){
+
+            sueldo= 1000.00;
+        }
+        return sueldo;
+    }
+
+    public double subsidio(int numero){
+        double sub=0.00;
+
+        if(numero>0){
+            sub=numero*50;
+        }else{
+            sub=0;
+        }
+        return sub;
+    }
+    public double atrasos(){
+        String atraso = et_atrasos.getText().toString();
+        double sueldo = Double.parseDouble(et_sueldo.getText().toString());
+        boolean tieneAtrasos = atraso.equalsIgnoreCase("sí");
+
+        double descuentoAtrasos = 0.0;
+        if (tieneAtrasos) {
+            descuentoAtrasos = -0.08 * sueldo;
+        }
+        return descuentoAtrasos;
+    }
+
 
     public void registrar(View view){
         BDHelper admin=new BDHelper(this,"parcial1.db",null,1);
@@ -46,7 +84,7 @@ public class MainActivity extends AppCompatActivity {
             registro.put("usu_hijos",hijos);
             registro.put("usu_estado",estado);
 
-            double sueldo;
+            /*double sueldo;
             if (cargo.equals("docente")) {
                 sueldo = 1000.0;
             } else if (cargo.equals("administrativo")) {
@@ -72,7 +110,7 @@ public class MainActivity extends AppCompatActivity {
             TextView tvSueldo = findViewById(R.id.txtSueldo);
             tvSueldo.setText("Sueldo: $" + sueldo);
 
-            registro.put("usu_sueldo", sueldo);
+            registro.put("usu_sueldo", sueldo);*/
 
             bd.insert("tblFuncionarios",null,registro);
             Toast.makeText(this, "REGISTRO EXITOSO", Toast.LENGTH_SHORT).show();
@@ -81,6 +119,10 @@ public class MainActivity extends AppCompatActivity {
             et_cargo.setText("");
             et_hijos.setText("");
             et_estado.setText("");
+            et_sueldo.setText(this.determinarSueldo(cargo)+"");
+            int numHijos=Integer.parseInt(hijos);
+            et_subsidio.setText(this.subsidio(numHijos)+"");
+            et_descuentoA.setText(this.atrasos()+"");
             bd.close();
         } else {
             Toast.makeText(this,"FAVOR INGRESAR TODOS LOS CAMPOS",Toast.LENGTH_SHORT).show();
